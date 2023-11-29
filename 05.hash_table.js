82dbf0f0e85cb19de748fc36a4e49216b51547a8
blair <blairzheng0312@gmail.com>
2023-11-29 23:27:25 +1100

    hash_table

--- 05.hash_table.js
+++ 05.hash_table.js
@@ -1,6 +1,11 @@
-// linked_list: connect nodes of data 
-// each node is store randomly in memory (array data is stored one next to another)
-// => do not require fixed space, suitable for dynamic data
+// direct addressing table: map data by keys in arrays
+// => drawback: large key range but small data small => waste space; key is not number => doesn't map in array
+
+// hash table can help with the issue => use hash function h(k) to map all values into a fixed size addressing table 
+// => drawback: hash collision: different key map into same address
+// => solution: 
+// 1) open addressing -> map to next opening address 
+// 2) chaining -> for keys mapped into same address, stored in linked-list
 
 class Node {
     constructor(item) {
@@ -99,15 +104,50 @@ class LinkedList {
     }
 }
 
-let linkedList = new LinkedList()
-linkedList.add(1)
-linkedList.add(2)
-linkedList.add(3)
-linkedList.add(4)
-linkedList.insert(5, 2)
-linkedList.remove(3)
+class HashTable {
+    constructor(size) {
+        this.size = size
+        this.hashTable = Array.from({ length: this.size }, () => new LinkedList())
+    }
 
-console.log(linkedList.length())
-console.log(linkedList.find(2).item)
-console.log(linkedList.findByValue(6))
-console.log(linkedList.toArray())
+    // design hash function to map keys to hash table
+    hash(k) {
+        return k % this.size
+    }
+
+    insert(k) {
+        // use hash function to find the index of hash table
+        let hashIndex = this.hash(k)
+        let hashList = this.hashTable[hashIndex]
+
+        // if key not exists, add the key to linked-list under the above hash index
+        if (hashList.findByValue(k) === -1) {
+            hashList.add(k)
+        }
+
+        // if key already exist, throw error 
+        else {
+            throw 'duplicated entry'
+        }
+    }
+
+    print() {
+        for (let item of this.hashTable) {
+            console.log(item.toArray())
+        }
+    }
+}
+
+let hashTable = new HashTable(10)
+
+hashTable.insert(2)
+hashTable.insert(3)
+hashTable.insert(12)
+hashTable.insert(22)
+// hashTable.insert(22)
+hashTable.print()
+
+
+
+
+
